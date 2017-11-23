cfb0d9f83ad4d9db7874fd3ce759a43ca0bb8b3b
kunnum <meeraj@servicesymphony.com>
2017-11-23 12:19:55 +0400

    More Scala docs

--- src/main/scala/com/retail/domain/User.scala
+++ src/main/scala/com/retail/domain/User.scala
@@ -1,8 +1,30 @@
 package com.retail.domain
 
+/**
+  * Represents a user of the system.
+  *
+  * @param userType Defines the type of the user.
+  * @param noOfYears No of years the user has been a customer.
+  */
 case class User(userType: UserType, noOfYears: Int)
 
+/**
+  * Represents a user type.
+  * @param label Label for the user.
+  */
 case class UserType(label: String)
+
+/**
+  * An employee user.
+  */
 object Employee extends UserType("EMPLOYEE")
+
+/**
+  * An affiliate user.
+  */
 object Affiliate extends UserType("AFFILIATE")
+
+/**
+  * A normal user.
+  */
 object Normal extends UserType("NORMAL")
