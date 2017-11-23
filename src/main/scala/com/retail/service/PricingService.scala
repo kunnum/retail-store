d0aa77dc80e6de143815f109cd85d5658387d856
kunnum <meeraj@servicesymphony.com>
2017-11-23 12:18:09 +0400

    Refining tests and adding Scala docs

--- src/main/scala/com/retail/service/PricingService.scala
+++ src/main/scala/com/retail/service/PricingService.scala
@@ -2,36 +2,82 @@ package com.retail.service
 
 import com.retail.domain.{Affiliate, Employee, Order, User}
 
+/**
+  * Entry point to the pricing service.
+  */
 object PricingService {
+
+  /**
+    * Prices an order based on the user type and characteristics of the order.
+    *
+    * @param user User who is placing the order.
+    * @param order Order that is being placed.
+    * @return The discounted price of the order.
+    */
   def applyDiscount(user: User, order: Order): BigDecimal = {
     val pricer = user match {
       case User(Employee, _) => EmployeePricer
       case User( Affiliate, _) => AffiliatePricer
-      case u => if (u.noOfYears > 2) LoyaltyPricer else NilPricer
+      case _ => LoyaltyPricer
     }
-    val price = pricer.price(order)
-    if (!order.isGrocery) bulkPrice(price) else price
+    val pricedOrder = pricer.price(user, order)
+    if (!order.isGrocery) bulkPrice(pricedOrder.value) else pricedOrder.value
   }
 
+  /*
+   * Apply a bulk price on all orders.
+   */
   private def bulkPrice(value: BigDecimal) = value * 0.95
 }
 
+/**
+  * An abstract pricer.
+  */
 trait Pricer {
-  def price(order: Order): BigDecimal
+  /**
+    * Prices the order.
+    * @param user User for whom order is being discounted.
+    * @param order Order to be priced.
+    * @return Discounted price.
+    */
+  def price(user: User, order: Order): Order
 }
 
+/**
+  * Pricing object for employees.
+  */
 object EmployeePricer extends Pricer {
-  def price(order: Order) = order.value * 0.7
+  /**
+    * Offers a 30% discount.
+    * @param user User for whom order is being discounted.
+    * @param order Order to be priced.
+    * @return Discounted price.
+    */
+  def price(user: User, order: Order) = order.copy(value = order.value * 0.7)
 }
 
+/**
+  * Pricing object for affiliates.
+  */
 object AffiliatePricer extends Pricer {
-  def price(order: Order) = order.value * 0.9
+  /**
+    * Offers 10% discount.
+    * @param user User for whom order is being discounted.
+    * @param order Order to be priced.
+    * @return Discounted price.
+    */
+  def price(user: User, order: Order) = order.copy(value = order.value * 0.9)
 }
 
+/**
+  * Loyalty based pricer.
+  */
 object LoyaltyPricer extends Pricer {
-  def price(order: Order) = order.value * 0.95
-}
-
-object NilPricer extends Pricer {
-  def price(order: Order) = order.value
+  /**
+    * Offers 10% discount.
+    * @param user User for whom order is being discounted.
+    * @param order Order to be priced.
+    * @return Discounted price.
+    */
+  def price(user: User, order: Order) = if (user.noOfYears > 2) order.copy(value = order.value * 0.95) else order
 }
--- src/test/scala/com/retail/service/PricingServiceSpec.scala
+++ src/test/scala/com/retail/service/PricingServiceSpec.scala
@@ -1,24 +1,63 @@
 package com.retail.service
 
-import com.retail.domain.{Employee, Order, User}
+import com.retail.domain._
 import org.scalatest.WordSpec
 
 /**
-  * Created by meeraj on 23/11/2017.
+  * Test case for the pricing service.
   */
 class PricingServiceSpec extends WordSpec with org.scalatest.Matchers {
 
   "Pricing service" should {
+
     "price employee users for non-grocery" in {
       val user = User(Employee, 10)
       val order = Order(200, false)
       PricingService.applyDiscount(user, order) shouldEqual 133
     }
+
     "price employee users for grocery" in {
       val user = User(Employee, 10)
       val order = Order(200, true)
       PricingService.applyDiscount(user, order) shouldEqual 140
     }
+
+    "price affiliate users for non-grocery" in {
+      val user = User(Affiliate, 10)
+      val order = Order(200, false)
+      PricingService.applyDiscount(user, order) shouldEqual 171
+    }
+
+    "price affiliate users for grocery" in {
+      val user = User(Affiliate, 10)
+      val order = Order(200, true)
+      PricingService.applyDiscount(user, order) shouldEqual 180
+    }
+
+    "price loyal users for non-grocery" in {
+      val user = User(Normal, 10)
+      val order = Order(200, false)
+      PricingService.applyDiscount(user, order) shouldEqual 180.5
+    }
+
+    "price loyal users for grocery" in {
+      val user = User(Normal, 10)
+      val order = Order(200, true)
+      PricingService.applyDiscount(user, order) shouldEqual 190
+    }
+
+    "price normal users for non-grocery" in {
+      val user = User(Normal, 1)
+      val order = Order(200, false)
+      PricingService.applyDiscount(user, order) shouldEqual 190
+    }
+
+    "not price normal users for grocery" in {
+      val user = User(Normal, 1)
+      val order = Order(200, true)
+      PricingService.applyDiscount(user, order) shouldEqual 200
+    }
+
   }
 
 }
